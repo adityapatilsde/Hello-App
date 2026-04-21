1d19af485f4b3147bdea9a0f5619312a24ac6aa4
aditya <adityapatilsde@gmail.com>
2026-04-21 19:07:02 +0530

    completed use case 5

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -4,16 +4,25 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        String message;
-
-        if (args.length > 0) {
-            String names = String.join(", ", args);
-            message = "Hello, " + names + "!";
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            message = "Hello, World!";
-        }
 
-        System.out.println(message);
+            StringBuilder names = new StringBuilder();
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    names.append(", ");
+                }
+                names.append(name);
+                first = false;
+            }
+
+            System.out.println("Hello, " + names + "!");
+        }
     }
 
 }
+
+
